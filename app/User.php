99c06fd889f7b2e7641b66560c63f53253552a7c
andreskrip <andreskrip@gmail.com>
2020-04-13 18:45:08 +0300

    Initial commit

---
+++ app/User.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends BaseModel
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public static function insert()
+    {
+        $user = new User();
+        $user->name = 'Random';
+        $user->email = 'email@example.com';
+        $user->password = 'qwerty';
+        $user->updatedAt = time() - 3600; //camelCase
+        $user->save();
+    }
+}
